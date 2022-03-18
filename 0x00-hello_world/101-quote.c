5a5541868b9047d7967cbbaabaa97b9bad2c3660
root <annasdickouu257@gmail.com>
2022-03-18 13:26:33 -0700

    final code update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void /*function that print with write function*/) 
+/**
+*function that print with write function 
+*/
+
+int main(void) 
 { 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
 	return (1);
